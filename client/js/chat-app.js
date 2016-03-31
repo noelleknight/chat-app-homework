4cf830d9e935f4d71a59c32023064b98d2ee91f1
Noelle Knight <noelle.knight@gmail.com>
2016-03-31 08:29:52 -0400

    cleaned up js code

--- client/js/chat-app.js
+++ client/js/chat-app.js
@@ -1,14 +1,14 @@
 // write code
 // call window.chat.init
 (function(chat) {
-      'use strict';
+  'use strict';
 
-var userInfo = {};
-var messageInfo = {};
+  var userInfo = {};
+  var messageInfo = {};
 
-    chat = chat || (window.chat = {});
+  chat = chat || (window.chat = {});
 
-    window.chat.init(function messageHandler(data) {
+  window.chat.init(function messageHandler(data) {
 
 
 
@@ -16,10 +16,6 @@ var messageInfo = {};
           .append('<p>')
           .find('p:last-child')
             .text(data.username + ' : ' + data.message);
-
-
-
-
     });
 
     $('#login').on('submit', function (event){
@@ -28,52 +24,53 @@ var messageInfo = {};
       var userName = $('input.username').val() ;
 
       $.ajax({
-                  url: '/login',
-                  type: 'POST',
-                  contentType: 'application/json',
-                  data: JSON.stringify({username: userName}),
-                  success: function userName (data){
-                    userInfo.username = data.username;
-                    userInfo.token = data.token;
-                    $('#login').hide();
-                    $('#chat').css( "display", "block");
+        url: '/login',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({username: userName}),
+        success: function userName (data){
+          userInfo.username = data.username;
+          userInfo.token = data.token;
+          $('#login').hide();
+          $('#chat').css( "display", "block");
 
-                        console.log( data );
-                  }
-
-
-
-              });
+          console.log( data );
+        }
 
 
 
-  });
-
-  $('#send-message').on('submit', function (event){
-    event.preventDefault();
-
-    var msgTxt =  $('input.message').val();
-
-    $.ajax({
-                url: '/chat',
-                type: 'POST',
-                contentType: 'application/json',
-                data: JSON.stringify({message: msgTxt}),
-                headers:
-                {
-                  authorization: userInfo.token
-                },
-                success: function message (data) {                  messageInfo.message = data.message;
-                      console.log( data );
-                }
-
-
-            });
+      });
 
 
 
-});
+    });
+
+    $('#send-message').on('submit', function (event){
+      event.preventDefault();
+
+      var msgTxt =  $('input.message').val();
+
+      $.ajax({
+        url: '/chat',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({message: msgTxt}),
+        headers:
+        {
+          authorization: userInfo.token
+        },
+        success: function message (data) {
+         messageInfo.message = data.message;
+         console.log( data );
+        }
+
+
+      });
 
 
 
-})(window.chat);
+    });
+
+
+
+  })(window.chat);
